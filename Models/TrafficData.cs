b0b0c08ed887e3918327e6f35e68207a7c8279e0
Jamie Liddicoat <jjliddi24@gmail.com>
2025-06-25 14:13:27 +1000

    Initial TrafficData 25/6
    Initial Traffic data class

---
+++ Models/TrafficData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartTrafficMonitor.Models
+{
+    public class TrafficData
+    {
+        //Creating Traffic Data values
+        public int Id { get; set; }
+        public string SensorId { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string MovementType { get; set; } //e.g pedestrian, vehicle, cyclist
+        public string Direction { get; set; }
+        public string Season { get; set; }
+        public int FootTrafficCount { get; set; }
+        public int VehicleCount { get; set; }
+        //Any more needed
+    }
+}
