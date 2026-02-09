a9967378aba365b353a2f30467c63f9a4e7fbef7
Jamie <jjliddi24@gmail.com>
2026-02-09 15:20:29 +1100

    23/09/25 - Program.CS update file
    Basic Auth Logic to update later once compelted.

--- Program.cs
+++ Program.cs
@@ -1,11 +1,21 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using SmartTrafficMonitor.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
 
 //Adding the services to the container.
 builder.Services.AddControllersWithViews();
+
+//Auth Foundation TO DO LATER!
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Account/Login";
+        options.AccessDeniedPath = "/Account/AccessDenied";
+    });
+    builder.Services.AddAuthorization();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -23,13 +33,18 @@ else
     app.UseDeveloperExceptionPage();
 }
 
+//redirect HTTP -> HTTPS
+app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 app.UseRouting();
 
-//Enables authorisation
-app.UseAuthorization();
+//Enables authentication
+app.UseAuthentication();
 
+//enables authorization
+app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
