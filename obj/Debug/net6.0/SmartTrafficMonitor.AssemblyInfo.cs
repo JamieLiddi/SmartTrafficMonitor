ed6fa09507c6f48bc6c1da899120b9221427e66d
gammaau <107395796+gammaau@users.noreply.github.com>
2026-02-16 16:02:07 +1100

    .

--- obj/Debug/net6.0/SmartTrafficMonitor.AssemblyInfo.cs
+++ obj/Debug/net6.0/SmartTrafficMonitor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartTrafficMonitor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42871eb236e44b23062f1876b7d426330f7eda10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3abdb22f4586abef96f6d3042b82c8751c8b7d6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartTrafficMonitor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartTrafficMonitor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
